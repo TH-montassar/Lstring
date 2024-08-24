9716a442c454cc3f519fc06cab4efa5c03bbd667
farhaniaziz <farhaniaziz285@gmail.com>
2024-08-24 19:21:38 +0100

    Ticket #1 :  create function Lstringcmp #1

--- src/lstring.c
+++ src/lstring.c
@@ -49,3 +49,32 @@ lstr_t *str_init(const char *str)
     lstr_cpy(pstr->buffer, str, 3, 4, 3);
     return pstr;
 }
+
+/**
+ * @brief Compare two strings.
+ *
+ * This function compares two strings character by character. It returns an integer
+ * indicating the difference between the ASCII values of the first unmatched characters.
+ * If the first string is greater, it returns a positive value; if the second string is
+ * greater, it returns a negative value. If both strings are equal, it returns 0.
+ *
+ * @param[in] str1 The first string to be compared.
+ * @param[in] str2 The second string to be compared.
+ *
+ * @return The difference between the ASCII values of the first differing characters.
+ *
+ * @retval 0 If the two strings are equal.
+ * @retval >0 If the first string is greater than the second string.
+ * @retval <0 If the first string is less than the second string.
+ */
+
+int Lstr_cmp(const lstr_t *str1 , const lstr_t *str2){
+
+    while (*str1 && (*str1 == *str2))
+    {
+        str1++;
+        str2++;
+    }
+    return (*str1 - *str2);
+
+}
